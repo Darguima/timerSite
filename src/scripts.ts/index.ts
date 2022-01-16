ab1563547fd5108731f87ab22590cf1d1b4d454a
Darguima <dsgdevbraga@gmail.com>
2022-01-16 23:28:51 +0000

    Calculing the values for the timer

--- src/scripts.ts/index.ts
+++ src/scripts.ts/index.ts
@@ -1,5 +1,9 @@
+const countDownDate = new Date() // Edit me  - 'Aug 07, 2021 22:30:00')
+const daysOnCountdownMonth = new Date(countDownDate.getFullYear(), countDownDate.getMonth() + 1, 0).getDate()
+
 function main () {
   swipeUpButton()
+  countdown()
 }
 
 main()
@@ -13,4 +17,52 @@ function swipeUpButton () {
       behavior: 'smooth'
     })
   })
+}
+
+function countdown () {
+  const counters = document.querySelectorAll<HTMLParagraphElement>('.counterText')
+
+  setInterval(() => {
+    const now = new Date()
+    if (now.getTime() - countDownDate.getTime() < 0) console.log('invalid date')
+
+    let seconds = now.getSeconds() - countDownDate.getSeconds()
+    let minutes = now.getMinutes() - countDownDate.getMinutes()
+    let hours = now.getHours() - countDownDate.getHours()
+    let days = now.getDate() - countDownDate.getDate()
+    let months = now.getMonth() - countDownDate.getMonth()
+    let years = now.getFullYear() - countDownDate.getFullYear()
+
+    if (seconds < 0) {
+      seconds += 60
+      minutes -= 1
+    }
+
+    if (minutes < 0) {
+      minutes += 60
+      hours -= 1
+    }
+
+    if (hours < 0) {
+      hours += 24
+      days -= 1
+    }
+
+    if (days < 0) {
+      days += daysOnCountdownMonth
+      months -= 1
+    }
+
+    if (months < 0) {
+      months += 12
+      years -= 1
+    }
+
+    counters[0].innerText = `${years} years`
+    counters[1].innerText = `${months} months`
+    counters[2].innerText = `${days} days`
+    counters[3].innerText = `${hours} hours`
+    counters[4].innerText = `${minutes} minutes`
+    counters[5].innerText = `${seconds} seconds`
+  }, 1000)
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,12 @@
 "use strict";
 
+var countDownDate = new Date(); // Edit me  - 'Aug 07, 2021 22:30:00')
+
+var daysOnCountdownMonth = new Date(countDownDate.getFullYear(), countDownDate.getMonth() + 1, 0).getDate();
+
 function main() {
   swipeUpButton();
+  countdown();
 }
 
 main();
@@ -14,4 +19,50 @@ function swipeUpButton() {
       behavior: 'smooth'
     });
   });
+}
+
+function countdown() {
+  var counters = document.querySelectorAll('.counterText');
+  setInterval(function () {
+    var now = new Date();
+    if (now.getTime() - countDownDate.getTime() < 0) console.log('invalid date');
+    var seconds = now.getSeconds() - countDownDate.getSeconds();
+    var minutes = now.getMinutes() - countDownDate.getMinutes();
+    var hours = now.getHours() - countDownDate.getHours();
+    var days = now.getDate() - countDownDate.getDate();
+    var months = now.getMonth() - countDownDate.getMonth();
+    var years = now.getFullYear() - countDownDate.getFullYear();
+
+    if (seconds < 0) {
+      seconds += 60;
+      minutes -= 1;
+    }
+
+    if (minutes < 0) {
+      minutes += 60;
+      hours -= 1;
+    }
+
+    if (hours < 0) {
+      hours += 24;
+      days -= 1;
+    }
+
+    if (days < 0) {
+      days += daysOnCountdownMonth;
+      months -= 1;
+    }
+
+    if (months < 0) {
+      months += 12;
+      years -= 1;
+    }
+
+    counters[0].innerText = "".concat(years, " years");
+    counters[1].innerText = "".concat(months, " months");
+    counters[2].innerText = "".concat(days, " days");
+    counters[3].innerText = "".concat(hours, " hours");
+    counters[4].innerText = "".concat(minutes, " minutes");
+    counters[5].innerText = "".concat(seconds, " seconds");
+  }, 1000);
 }
