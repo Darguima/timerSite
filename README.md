2e35a1ff28652729a6a994ddeae93d02f343cd45
Darguima <dsgdevbraga@gmail.com>
2022-03-13 16:01:05 +0000

    Images spawning

--- README.md
+++ README.md
@@ -5,17 +5,18 @@ A website with a timer. This is an adaptable website created for use with any da
 ### Editing
 
 All variables that you may want edit are in the top of `src/scripts/index.ts`.
-Change what you want on `src/index.html` and `src/styles/index.css`
+
+Change what you want on `src/index.html` and `src/styles/index.css`.
+
+Change the images on `assets/images/`. To use them in JS they need to be named `image{number}.jpg` and edit `imagesAmount`.
 
 ### Building
 
 ```console
 $ npm build
-```
 
 or
 
-```console
 $ yarn build
 ```
 
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,13 @@
 const countDownDate = new Date('Jan 02, 2022 16:23:04')
+const imagesAmount = 7
+
 const daysOnCountdownMonth = new Date(countDownDate.getFullYear(), countDownDate.getMonth() + 1, 0).getDate()
+const { clientWidth: vw, clientHeight: vh } = document.documentElement
 
 function main () {
+  images()
+  images()
+  images()
   timer()
 }
 
@@ -56,4 +62,32 @@ function timer () {
     counters[4].innerText = `${minutes} minutes`
     counters[5].innerText = `${seconds} seconds`
   }, 1000)
+}
+
+function images () {
+  const imageContainer = document.getElementById('imageContainer')!
+
+  const image = document.createElement('img')
+
+  image.classList.add('image')
+  imageContainer.appendChild(image)
+
+  image.src = `assets/images/image${getRandomInt(imagesAmount, 1)}.jpg`
+
+  image.style.top = `${getRandomInt(vh - image.height / 2, -image.height / 2)}px`
+  image.style.left = `${getRandomInt(vw - image.width / 2, -image.width / 2)}px`
+
+  image.style.opacity = '1'
+
+  setTimeout(() => {
+    image.style.opacity = '0'
+    setTimeout(() => {
+      image.remove()
+      images()
+    }, 1000)
+  }, getRandomInt(5000, 3500))
+}
+
+function getRandomInt (max: number, min: number = 0) {
+  return Math.floor(Math.random() * max) + min
 }
